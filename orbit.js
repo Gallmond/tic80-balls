801182d62a1a6518e00087ba2b7622d6ad57d45e
gavin <gallmond@gmail.com>
2021-03-28 15:31:07 +0100

    .

--- orbit.js
+++ orbit.js
@@ -1,18 +1,18 @@
 // title:  gravity balls
 // author: gav
-// desc:   just playing with gravity and frictionless conservation of mass
+// desc:   just playing with gravity and elastic collision
 // script: js
 
 /**
  * UP key to add a new body
- * DOWN key to remove the oldest body
+ * DOWN key to remove the oldest body (hold SHIFT + DOWN to remove one per frame)
+ * LEFT/RIGHT keys to switch through bodies to follow
  * LEFT CLICK to follow the nearest body or toggle the body following
  * RIGHT CLICK to pin a body (prevent position update)
+ * M to toggle zoomed out view of all bodies regardless of distance 
+ * B key to toggle boundary walls (causes problems's if they snap-back inside eachother) 
  * R key to restart
- * B key to force bodies to stay inside the rectangle (causes NaN's if they snap-back inside eachother)
- * TODO M to show a 'map' of all bodies regardless of distance
- */
-
+  */
 
 var win_w = 240
 var win_h = 136 
@@ -59,21 +59,17 @@ var OrbitalBody = function(mass,radius,spawn_x,spawn_y,pinned){
 	this.pinned = pinned ? pinned : false;
 	this.c = 5;
 
-	this.init = {
-		m: mass,
-		r: radius,
-		x: spawn_x,
-		y: spawn_y,
-		v_x: 0,
-		v_y: 0,
-		pinned: pinned
-	};
-
 	this.step = 1000/120;
 
 	this.update_velocity = function(){
 
-		// gravity stretches over the whole universe! Check all bodies
+		// some error correction for when the maths breaks
+		if(this.v_x === undefined || Number.isNaN(this.v_x)) this.v_x = 0;
+		if(this.v_y === undefined || Number.isNaN(this.v_y)) this.v_y = 0;
+		if(this.x === undefined || Number.isNaN(this.x)) this.x = win_w/2;
+		if(this.y === undefined || Number.isNaN(this.y)) this.y = win_h/2;
+
+		// gravity stretches over the whole universe! Check all bodies. In an actual implementation I'd probably limit this
 		for(var i = 0, l = bodies.length; i < l; i++){
 			var b = bodies[i];
 			if(b === this) continue; // skip me though
@@ -100,7 +96,7 @@ var OrbitalBody = function(mass,radius,spawn_x,spawn_y,pinned){
 				continue;
 			}
 			
-			if(draw_connecting_lines && !key(13)) line(this.x-offset_x,this.y-offset_y,b.x-offset_x,b.y-offset_y,14);
+			if(draw_connecting_lines && !mapmode) line(this.x-offset_x,this.y-offset_y,b.x-offset_x,b.y-offset_y,14);
 
 			// this is the c^2 in pythagoras' c^2 = a^2 + b^2
 			var sqrDist = ((b.x-this.x) * (b.x-this.x)) + ((b.y-this.y) * (b.y-this.y))
@@ -137,11 +133,6 @@ var OrbitalBody = function(mass,radius,spawn_x,spawn_y,pinned){
 			this.v_y+= acceleration_y * this.step;
 		}
 
-		if(this.pinned){
-			// this.v_x = 0;
-			// this.v_y = 0;
-		}
-
 	}
 
 	this.update_position = function(){
@@ -159,15 +150,18 @@ var OrbitalBody = function(mass,radius,spawn_x,spawn_y,pinned){
 	}
 }
 
-
+// some variation on starting stuff
 function mr(){
 	return (Math.random() - 0.5) * 3;
 }
 
-function rand(min,max){ // returns random int from min to max inclusive
+// returns random int from min to max inclusive
+function rand(min,max){ 
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// randomly add a new body in the starting area (if there is space for it)
+// if no space is found in a second just cancel
 function addBody(){
 	var s = time();
 	var is_space = false;
@@ -183,18 +177,23 @@ function addBody(){
 			if(d <= r + b.r) is_space = false;
 		}
 	}
-	bodies.push(new OrbitalBody(m,r,x,y));
+	if(is_space) bodies.push(new OrbitalBody(m,r,x,y));
 }
 
+function intFact(num)
+{
+    var rval=1;
+    for (var i = 2; i <= num; i++) rval = rval * i;
+    return rval;
+}
+
+// some intial starting bodies
 var sizes = [
 	rand(7,30),
 	rand(7,30),
 	rand(7,30),
 ];
-
-// init some bodies to start with
 // OrbitalBody(mass,radius,spawn_x,spawn_y,pinned).
-var main = false;
 var bodies = [
 	new OrbitalBody(sizes[0], sizes[0], 56+mr(),35+mr()),
 	new OrbitalBody(sizes[1], sizes[1], 215+mr(),113+mr()),
@@ -202,14 +201,27 @@ var bodies = [
 ];
 
 
-main.c = 6;
-main.is_main = true;
+// set the body to follow with the camera
+var main = false;
+function setMain(body){
+	if(body !== main){
+		main = body;
+		main.c = 6;
+		main.is_main = true;
+		main.is_main = true;
+	}else{
+		main.c = 5;
+		main.is_main = false;
+		main=false;
+	}
+}
 
 var offset_x = 0
 var offset_y = 0
 
 var downheld = false;
 var hidelines = false;
+var mapmode = false;
 var t = time();
 function TIC()
 {
@@ -223,7 +235,6 @@ function TIC()
 		offset_y = main.y - win_h / 2;
 	}
 
-
 	// 02 = B
 	if(keyp(2)){
 		force_in_box = !force_in_box;
@@ -236,12 +247,27 @@ function TIC()
 	if(keyp(58)){
 		addBody();
 	}
-	//59 = DOWN
-	if(keyp(59)){
+	//59 = DOWN, 64 = SHIFT
+	if(keyp(59) || (key(64) && key(59))){
 		bodies.shift();
 	}
-	//60 = LEFT
-	//61 = RIGHT
+	// 13 = M
+	if(keyp(13)) mapmode = !mapmode;
+	// 60 = LEFT, 61 = RIGHT
+	if(keyp(60) || keyp(61)){
+		if(main){
+			var changed = false;
+			for(var i = 0, l = bodies.length; i < l && !changed; i++){
+				//find current main
+				if(main===bodies[i]){
+					var new_i = keyp(60) ? i-1 : i+1;
+					new_i = new_i<0 ? new_i = bodies.length-1 : new_i%bodies.length;
+					setMain(bodies[new_i]);
+					changed = true;
+				}
+			}
+		}
+	}
 
 	// update all bodies
 	if(t > 1000){
@@ -259,29 +285,29 @@ function TIC()
 			dist = p.dist;
 			overlap = p.overlap;
 
-			//TODO recomment this. It's not clear to me what's going on
-
-			// normalise
+			// from the distance line between the two circles, turn it into a normalised vector
 			var nx = (b.x - a.x) / dist			
 			var ny = (b.y - a.y) / dist			
 
-			// tangent
+			// now create a tangent line from this normalised vector
 			var tx = ny*-1;
 			var ty = nx;
 
-			// dot products tangent
-			var dptan1 = a.v_x * tx + a.v_y * ty
-			var dptan2 = b.v_x * tx + b.v_y * ty
+			// tangential response, which direction to fire "sideways"
+			// use dot products to get "angle similarity" of body against the tangent line. It will be between -1 and 1 (for normalised vectors)
+			var dptan1 = a.v_x * tx + a.v_y * ty; // body a
+			var dptan2 = b.v_x * tx + b.v_y * ty; // body b 
 
-			// dot products normal
-			var dpnorm1 = a.v_x * nx + a.v_y * ny
-			var dpnorm2 = b.v_x * nx + b.v_y * ny
+			// do the same thing to get the dot products against the normal line.
+			// I understand what is happening here but not how to visualise it
+			var dpnorm1 = a.v_x * nx + a.v_y * ny; // body a
+			var dpnorm2 = b.v_x * nx + b.v_y * ny; // body b 
 
 			// conserve 2d momentum
-			var m1 = (dpnorm1 * (a.m - b.m) + 2 * b.m * dpnorm2) / (a.m + b.m)
-			var m2 = (dpnorm2 * (b.m - a.m) + 2 * a.m * dpnorm1) / (a.m + b.m)
+			var m1 = (dpnorm1 * (a.m - b.m) + 2 * b.m * dpnorm2) / (a.m + b.m); // body a
+			var m2 = (dpnorm2 * (b.m - a.m) + 2 * a.m * dpnorm1) / (a.m + b.m); // body b 
 
-			// alter velocity
+			// alter velocity in both tangential and normal responses
 			a.v_x = tx * dptan1 + nx * m1;
 			a.v_y = ty * dptan1 + ny * m1;
 			b.v_x = tx * dptan2 + nx * m2;
@@ -293,29 +319,20 @@ function TIC()
 		for(var i = 0, l = bodies.length; i < l; i++){
 			bodies[i].update_position();
 		}
-	
 
 	}else{
-		print(((1000-t)/1000).toFixed(2),50,50,0);
+		print(((1000-t)/1000).toFixed(2),50,50,0); // draw countdown
 	}
 
-
-		// draw 'main' window rectangle
-		rectb(0 - offset_x,0 - offset_y,win_w,win_h, force_in_box ? 2 : 14);
-
-	// 13 = M
-	if(key(13)){
+	// draw map mode circles and lines
+	if(mapmode){
 		var points = [];
 
 		// get the bbox for these points
 		var low_x=undefined,high_x=undefined,low_y=undefined,high_y=undefined;
-
 		for(var i = 0, l = bodies.length; i < l; i++){
 			var b = bodies[i];
-			points.push([b.x,b.y]);
-
-			//TODO reset to 0 here
-
+			points.push([b.x,b.y,b.r]);
 			if(low_x === undefined || b.x < low_x){
 				low_x = b.x
 			}
@@ -328,89 +345,100 @@ function TIC()
 			if(high_y === undefined || b.y > high_y){
 				high_y = b.y
 			}
-
 		}
 
+		// calculate the scale needed to resize the box to the window
 		var x_range = high_x - low_x;
 		var y_range = high_y - low_y;
 		var x_scale = 1;
 		var y_scale = 1;
-		if(x_range > win_w) x_scale = x_range / win_w
-		if(y_range > win_h) y_scale = y_range / win_h
+		if(x_range > win_w) x_scale = win_w / x_range
+		if(y_range > win_h) y_scale = win_h / y_range
 
+		scale_to_use = Math.min(win_w / x_range, win_h / y_range, 1)
+		x_scale = scale_to_use;
+		y_scale = scale_to_use;
 
-		var n = 20;
-		print("x_range["+x_range.toFixed(1)+"] x_scale["+x_scale.toFixed(1)+"]",5,20,5);
-		
-		print("points[0][0] " + String(points[0][0]),5,27,5);
-		print("points[0][0]* x_scale " + String(points[0][0]* x_scale),5,33,5);
-
-		while (points.length > 0) {
-			var p = points.shift();
-
-
-
-			var this_x = (p[0] / x_scale) - low_x;
-			var this_y = (p[1] / y_scale) - low_y;
-
-			//TODO how to translate to 0 (ie keep on scree)
-
-			//TODO how to scale proper and maintain aspect ratio
-
-			pix(this_x, this_y, 12);
-
+		if(false){
+			var n = 7;
+			print("x_range["+x_range.toFixed(1)+"] x_scale["+x_scale.toFixed(1)+"]",5,20+(n+=7),5);
+			print("y_range["+y_range.toFixed(1)+"] y_scale["+y_scale.toFixed(1)+"]",5,20+(n+=7),5);
+			print("points[0][0] " + String(points[0][0]),5,20+(n+=7),5);
+			print("points[0][0]* x_scale " + String(points[0][0]* x_scale),5,20+(n+=7),5);
 		}
 
+		// draw lines and collect info for circles
+		var circs_to_draw = [];
+		while (points.length > 0) {
+			var p = points.shift();
+			var this_x = (p[0] * x_scale) - (low_x * x_scale);
+			var this_y = (p[1] * y_scale) - (low_y * y_scale);
+
+			for(var i = 0, l = points.length; i < l; i++){
+				var t_x = (points[i][0] * x_scale) - (low_x * x_scale);
+				var t_y = (points[i][1] * y_scale) - (low_y * y_scale);
+				line(this_x,this_y,t_x,t_y,14)
+			}
+			circs_to_draw.push([this_x, this_y ,p[2] * scale_to_use, 5])
+		}
+
+		// draw reference rectangle
+		rectb(0 - low_x * x_scale,0 - low_y * y_scale,win_w * x_scale,win_h * y_scale,force_in_box ? 2 : 13);
+
+		// draw resized circles
+		while(circs_to_draw.length > 0){
+			var c = circs_to_draw.shift();
+			circb(c[0],c[1],c[2],c[3]);
+			pix(c[0],c[1], 12);
+		}
 
 	}else{
 
+			// draw main rectangl
+		rectb(0 - offset_x,0 - offset_y,win_w,win_h, force_in_box ? 2 : 13);
+
 		// draw bodies
-	for(var i = 0, l = bodies.length; i < l; i++){
-		var b = bodies[i];
+		for(var i = 0, l = bodies.length; i < l; i++){
+			var b = bodies[i];
+			
+			// draw circle
+			circb(b.x - offset_x,b.y - offset_y,b.r,b.c);
 		
-		// draw circle
-		circb(b.x - offset_x,b.y - offset_y,b.r,b.c);
-	
-		// mass and radius
-		var s1 = "m:"+String(b.m)+" r:"+String(b.r);
-		var sl1 = print(s1, -100,-100,0,false,1,true);
-		print(s1, b.x - offset_x - (sl1*0.5), b.y - offset_y - 7,13,false,1,true);
+			// mass and radius
+			var s1 = "m:"+String(b.m)+" r:"+String(b.r);
+			var sl1 = print(s1, -100,-100,0,false,1,true);
+			print(s1, b.x - offset_x - (sl1*0.5), b.y - offset_y - 7,13,false,1,true);
 
-		// x and y position
-		var s2 = "x:"+b.x.toFixed(1)+" y:"+b.y.toFixed(1);
-		var sl2 = print(s2, -100,-100,0,false,1,true);
-		print(s2, b.x - offset_x - (sl2*0.5), b.y - offset_y+2,13,false,1,true);
+			// x and y position
+			var s2 = "x:"+b.x.toFixed(1)+" y:"+b.y.toFixed(1);
+			var sl2 = print(s2, -100,-100,0,false,1,true);
+			print(s2, b.x - offset_x - (sl2*0.5), b.y - offset_y+2,13,false,1,true);
 
-		if(b.pinned){
-			var sl = print("pinned",-100,-100,1,false,1,true);
-			print("pinned",b.x - offset_x - (sl/2),b.y - offset_y,b.c,false,1,true);
-		} 
+			// pinned label
+			if(b.pinned){
+				var sl = print("pinned",-100,-100,1,false,1,true);
+				print("pinned",b.x - offset_x - (sl/2),b.y - offset_y,b.c,false,1,true);
+			} 
 
-		if(visualise_velocity){
-			var vx = b.x - offset_x + (b.v_x*-1) * 150; // exaggerated for display
-			var vy = b.y - offset_y + (b.v_y*-1) * 150; // exaggerated for display
-			// create a right angle triangle with the hypotenuse acting as our 'arrow'
-			line(
-				b.x - offset_x	, b.y - offset_y,
-				vx,		vy,
-				3
-			)
+			// velocity arrows
+			if(visualise_velocity){
+				var vx = b.x - offset_x + (b.v_x*-1) * 150; // exaggerated for display
+				var vy = b.y - offset_y + (b.v_y*-1) * 150; // exaggerated for display
+				// create a right angle triangle with the hypotenuse acting as our 'arrow'
+				line(
+					b.x - offset_x	, b.y - offset_y,
+					vx,		vy,
+					3
+				)
+			}
 		}
-
 	}
-
-	}
-
-
-	
-
-
-
 }
 
 var mouse_clicked = time();
 function OVR(){
 
+	// 'camera' offset from main
 	offset_x = 0;
 	offset_y = 0;
 	if(main){
@@ -427,19 +455,13 @@ function OVR(){
 	msx=mouseData[5], // scrollx
 	msy=mouseData[6]; // scrolly
 
-	print("mx["+String(mx)+"] my["+String(my)+"]",2,win_h-7,0);
+	// print("mx["+String(mx)+"] my["+String(my)+"]",2,win_h-7,0);
 
-	if(key(28)){ // the 1 key
-		if(main){
-			main.x = win_w / 2;
-			main.y = win_h / 2;
-		}
-	}
 	
 	if((ml || mr) && time()-mouse_clicked > 333){
 		mouse_clicked = time();
 
-		// get nearest celestial body
+		// get nearest celestial body to mouse on screen
 		var nearest = false;
 		var nearest_dist = false;
 		for(var i = 0, l = bodies.length; i < l; i++){
@@ -456,21 +478,7 @@ function OVR(){
 
 		if(nearest){
 			if(ml){
-				if(nearest === main){
-					main.c = 5
-					main.is_main = false
-					main = false;
-				}else if(main){
-					main.c = 5
-					main.is_main = false
-					main = nearest
-					main.c = 6
-					main.is_main = true
-				}else{
-					main = nearest
-					main.c = 6
-					main.is_main = true
-				}
+				setMain(nearest);
 			}
 			if(mr){
 				nearest.pinned = !nearest.pinned;
@@ -483,9 +491,14 @@ function OVR(){
 
 	}
 
-	print("collisions["+String(collisions)+"]",2,2,0);
-	print("bodies["+String(bodies.length)+"]",2,2+7,0);
-	
+	// how many checks are being done?
+	var checks = intFact(bodies.length);
+	checks = checks === 1 ? 0 : checks;
+
+	print("bodies["+String(bodies.length)+"]",2,2,0);
+	print("collisions["+String(collisions)+"]",2,2+7,0);
+	print("velocity updates / frame",2,2+14,0);
+	print("["+String(checks)+"]",2,2+21,0);
 
 }
 
